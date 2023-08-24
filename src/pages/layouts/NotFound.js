fc4c5378ee2f1a99d63a0057de5d7f5629129e4d
dportugaln <dportugaln@gmail.com>
2023-08-24 12:22:20 -0300

    Solucionado problema con ruta a fetchlist y tamaño de image de notfound

--- src/pages/layouts/NotFound.js
+++ src/pages/layouts/NotFound.js
@@ -1,27 +1,3 @@
-/* import React from 'react'
-
-import NotFoundImg from '../../assets/images/404.png';
-
-
-
-
-const NotFound = () => { return (
-<>
-
-<img src={NotFoundImg}/>
-
-</>
-
-)
-
-}
-
-
-
-
-export default NotFound
- */
-
 import React from 'react';
 import NotFoundImg from '../../assets/images/404.png';
 
